d872266eaa4b582fc6e503d8b0a7aada7ad4f4f3
谢良强 <614000587@qq.com>
2019-07-01 10:22:12 +0800

    1

--- src/session/sess.go
+++ src/session/sess.go
@@ -1,45 +1,175 @@
 package main
 
-import "sync"
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"io"
+	"sync"
+	"time"
+)
 
-//session 管理器
-type Manager struct {
-	cookieName string  //private 客户端cookie名称 如jsessionID
-	lock sync.Mutex    //互斥锁
-	provider Provider  //session数据存储驱动  文件、内存、数据库
-	maxlifetime int64  //session 垃圾回收时间
+//定义session的接口
+type Session interface{
+	Set(key,value interface{})	
+	Get(key interface{}) interface{}	
+    Remove(key interface{}) error
+    GetId() string
+} 
+
+//定义一个实现内存session接口的结构体
+type SessionFromMemory struct{
+	sid 	string 		//每个cookie对应的sessionID值
+	lock	sync.Mutex 	//互斥锁
+	lastAccessedTime	time.Time	//用户最后的访问时间
+	maxAge	int64		//存活时间 单位秒
+	data	map[interface{}]interface{} 	//每个用户对应的session值/列表
 }
 
-//session 数据驱动接口
-type Provider interface {
-	SessionInit(sid string) (Session,error)  //初始化session驱动
-	SessionRead(sid string) (Session,error)  //读取session
-	SessionDestroy(sid string) error         //删除session
-	SessionGC(maxlifetime int64)             //设置session 定时过期
+//内存seesion结构体实现session接口
+func (s SessionFromMemory) Set(key,value interface{}){
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	s.data[key] = value
+}
+func (s SessionFromMemory) Get(key interface{}) interface{} {
+	if value := s.data[key];value != nil {
+		return value
+	}
+	return nil
+}
+func (s SessionFromMemory) Remove(key interface{}) error {
+	if value := s.data[key];value != nil {
+		delete(s.data,key)
+	}
+	return nil
+}
+func (s SessionFromMemory) GetId() string {
+	return s.sid
 }
 
-//session 存取接口
-type Session interface {
-	Set(key,value interface{}) error   //存数据
-	Get(key interface{}) interface{}  //读取数据
-	Delete(key interface{}) 		  //删除数据
-	SessionId() string				  //返回当前的sessionID
-	GC() 							  //删掉用户的所有json
+//获取新的session
+func newSessionFromMemory() *SessionFromMemory {
+	return &SessionFromMemory{
+		data:   make(map[interface{}]interface{}),
+        maxAge: 1800, //默认30分钟
+	}
 }
 
-//驱动列表
-var providers = make(map[string]Provider)
 
-//驱动注册
-func Register(name string,provide Provider)  {
+
+//定义session存储的驱动的接口
+type Storage interface {
+	//初始化一个session，id根据需要生成后传入
+	InitSession(sid string,maxAge int64) (Session,error)
+	//销毁session
+    DestroySession(sid string) error
+    //回收
+    GCSession()
+}
+
+//定义实现内存session接口的管理器---即所有session的集合
+type FromMemory struct {
+	lock sync.Mutex
+	sessions map[string]Session
+}
+//实例化一个内存session实现
+func newFromMemory() *FromMemory {
+    return &FromMemory{
+        sessions: make(map[string]Session, 0),
+    }
+}
+
+//初始化一个session 并将其加入管理列表
+func (ms FromMemory) InitSession(sid string,maxAge int64) (Session,error) {
+	ms.lock.Lock()
+	defer ms.lock.Unlock()
 	
+	newSession := newSessionFromMemory()
+	newSession.sid = sid
+	if maxAge > 0 {
+		newSession.maxAge = maxAge
+	}
+	newSession.lastAccessedTime = time.Now()
+	ms.sessions[sid] = newSession
+	return newSession,nil
+}
+//销毁某个用户的session
+func (ms FromMemory) DestroySession(sid string) error {
+	ms.lock.Lock()
+	defer ms.lock.Unlock()
+	if session :=ms.sessions[sid];session != nil {
+		delete(ms.sessions,sid)
+	}
+	return nil
+}
+//清除超时的session -- 一段时间内未使用的sesiion
+func (ms FromMemory)GCSession()  {
+	sessions := ms.sessions
+	if len(sessions) < 1 {
+		return
+	}
+	for k,v := range sessions {
+		t := v.(*SessionFromMemory).lastAccessedTime.Unix() + v.(*SessionFromMemory).maxAge
+		if t < time.Now().Unix() {
+			delete(ms.sessions,k)
+		}
+	}
+
 }
 
-//创建新的session管理器
-func NewManager(providerName,cookieName string,maxlifetime int64)  {
+//控制session storage cookie的管理器
+type SessionManager struct {
+	cookieName string   //客户端cookie的名字
+	storage Storage    //session驱动
+	maxAge int64 		//存活时间
+	lock sync.Mutex     //互斥锁
+}
+
+//定时清理数据
+func (m *SessionManager) GC() {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	m.storage.GCSession()
+	//在多长时间后执行匿名函数，这里指在某个时间后执行GC
+	time.AfterFunc(time.Duration(m.maxAge*10), func() {
+		m.GC()
+	})
+}
+//生成一定长度的随机数
+func (m *SessionManager) randomId() string {
+	b := make([]byte, 32)
+	if _, err := io.ReadFull(rand.Reader, b); err != nil {
+		return ""
+	}
+	//加密
+	return base64.URLEncoding.EncodeToString(b)
+}
+
+//通过ID获取session
+func (m *SessionManager) GetSessionById(sid string) Session {
+	session := m.storage.(*FromMemory).sessions[sid]
+	return session
+}
+
+func NewSessionManager() *SessionManager {
+	sessionManager := &SessionManager{
+		cookieName: "lzy-cookie",
+		storage:    newFromMemory(), //默认以内存实现
+		maxAge:     1800,         //默认30分钟
+	}
+	go sessionManager.GC()
+
+	return sessionManager
+}
+
+
+
+
+//https://studygolang.com/articles/12856
+
+
+
+
+
 	
-}
-
-func main()  {
-
-}
